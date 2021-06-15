4fd6d7c52db7a1fa49eec6093fcbe2fe1ba2b658
Ajay554 <43260581+Ajay554@users.noreply.github.com>
2021-06-15 14:00:34 +0000

    Updated code

--- index.js
+++ index.js
@@ -25,7 +25,14 @@ const epochHumanDate = function(option){
     const epochTime = readLineSync.question('\nPlease enter epoch time, ex - 1611041456000:\n');
     console.log(new Date(epochTime*1000).toUTCString());
   }else{
-
+    const year = readLineSync.question("Please enter the year : \n");
+    const month = readLineSync.question("Please enter the month : \n");
+    const date = readLineSync.question("Please enter the date : \n");
+    const hour = readLineSync.question("Please enter the hour : \n");
+    const min = readLineSync.question("Please enter the minute : \n");
+    const sec = readLineSync.question("Please enter the seconds \n: ");
+    const humanDate = new Date(Date.UTC(year,month,date,hour,min,sec));
+    console.log(Math.floor(humanDate/1000.0));
   }
 }
 const hashing = function(option){
@@ -38,6 +45,45 @@ const hashing = function(option){
     console.log(crypto.createHash("sha256").update(uri).digest('hex'));
   }else{
     console.log(crypto.createHash("sha512").update(uri).digest('hex'));
+  }
+}
+const hex = function (rgb) { 
+  let hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+};
+function rgb(h) {
+  let r = 0, g = 0, b = 0;
+
+  // 3 digits
+  if (h.length == 4) {
+    r = "0x" + h[1] + h[1];
+    g = "0x" + h[2] + h[2];
+    b = "0x" + h[3] + h[3];
+
+  // 6 digits
+  } else if (h.length == 7) {
+    r = "0x" + h[1] + h[2];
+    g = "0x" + h[3] + h[4];
+    b = "0x" + h[5] + h[6];
+  }
+  
+  return "rgb("+ +r + "," + +g + "," + +b + ")";
+}
+const rgbHex = function(option){
+  if(option == '1'){
+    const red = readLineSync.question('\nEnter the red value:\n');
+    const green = readLineSync.question('\nEnter the green value:\n');
+    const blue = readLineSync.question('\nEnter the blue value:\n');
+    const rgbToHex = (r, g, b) => '#' + 
+    hex(r) + hex(g) + hex(b);
+    
+    console.log(rgbToHex(red, green, blue));
+  }else{
+    const rgbValue = readLineSync.question('\nEnter rgb value:\n');
+    console.log(rgb(rgbValue));
   }
 }
 const utilityContinue = function(){
@@ -105,7 +151,31 @@ const utilityApptoRun = function(selectedOption){
     case '4':
       option = readLineSync.question('\nselect 1 for humanDate, or 2 for epoch Time\n');
       epochHumanDate(option);
-      decision = readLineSync.question('\nEnter 1 to continue, 2 to go to the main menu, 3 to exit\n')
+      decision = readLineSync.question('\nEnter 1 to continue, 2 to go to the main menu, 3 to exit\n');
+      if(decision == '1'){
+        continue;
+      }
+      else if(decision == '2'){
+        utilityContinue();
+      }
+      else{
+        flag = false;
+        break;
+      }
+    case '6':
+      option = readLineSync.question('\nselect 1 for RGB => Hex, or 2 for Hex => RGB Time\n');
+      rgbHex(option);
+      decision = readLineSync.question('\nEnter 1 to continue, 2 to go to the main menu, 3 to exit\n');
+      if(decision == '1'){
+        continue;
+      }
+      else if(decision == '2'){
+        utilityContinue();
+      }
+      else{
+        flag = false;
+        break;
+      }
   }
   }while(flag)
   
